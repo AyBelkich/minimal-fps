e5772d14ee484721a9c4774a09add466a497474c
AyBelkich <belkich.ayoub@gmail.com>
2026-06-10 21:43:26 +0200

    Spawn targets only at empty spawn points.

--- scripts/main.gd
+++ scripts/main.gd
@@ -23,39 +23,40 @@ func add_score(amount: int):
 	if score >= target_goal:
 		win_label.visible = true
 
-func spawn_targets():
+func spawn_target_at(spawn_point: Node3D) -> void:
+	var target = target_scene.instantiate()
 
+	target.id = target_id
+	target_id += 1
+
+	spawn_point.add_child(target)
+
+	target.position = Vector3.ZERO
+	target.target_died.connect(on_target_died)
+
+func spawn_targets() -> void:
 	var spawn_points = $Targets/SpawnPoints.get_children()
 
-	for spawn in spawn_points:
-
-		var target = target_scene.instantiate()
-		target.id = target_id
-		target_id += 1
-
-		target.position = spawn.position
-
-		target.target_died.connect(on_target_died)
-
-		$Targets.add_child(target)
+	for spawn_point in spawn_points:
+		spawn_target_at(spawn_point)
 
 func respawn_target():
-
 	await get_tree().create_timer(2.0).timeout
 
 	var spawn_points = $Targets/SpawnPoints.get_children()
+	var empty_spawn_points = []
+	
+	for spawn_point in spawn_points:
+			if spawn_point.get_child_count() == 0:
+				empty_spawn_points.append(spawn_point)
 
-	var random_spawn = spawn_points.pick_random()
+	if empty_spawn_points.is_empty():
+		print("There are no empty spawn points!")
+		return
 
-	var target = target_scene.instantiate()
+	var random_spawn = empty_spawn_points.pick_random()
+	spawn_target_at(random_spawn)
 
-	target.position = random_spawn.position
-
-	target.target_died.connect(on_target_died)
-
-	$Targets.add_child(target)
-
-
-func on_target_died(dead_target_id: int):
+func on_target_died(dead_target_id: int) -> void:
 	print("target %s died" % dead_target_id)
 	respawn_target()
