1d58d0a4bab729ed4740b870c4ac89ad6e2ddfa4
AyBelkich <belkich.ayoub@gmail.com>
2026-06-12 11:53:30 +0200

    Targets movement relative to their spawnpoint

--- scripts/target.gd
+++ scripts/target.gd
@@ -4,12 +4,24 @@ signal target_died(dead_target_id: int)
 
 var id = 0
 var health := 4
+var move_direction := 1
+var move_speed := 1.5
 
 @onready var mesh: MeshInstance3D = $DummyTarget
 
 func _ready():
 	mesh.material_override = StandardMaterial3D.new()
 	mesh.material_override.albedo_color = Color.GREEN
+
+func _process(delta):
+
+	position.x += move_direction * move_speed * delta
+
+	if position.x > 2:
+		move_direction = -1
+
+	if position.x < -2:
+		move_direction = 1
 
 func take_damage(amount: int):
 	health -= amount
